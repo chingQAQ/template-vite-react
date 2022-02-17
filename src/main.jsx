e1f9e99326a3ed16983d100e3e8119f130a49949
AionChiu <30471173+chingQAQ@users.noreply.github.com>
2022-02-17 15:44:00 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 import App from './App';
 
 const rootElement = document.getElementById("root");
-if (!element) {
+
+if (!rootElement) {
   throw new Error("should include #root in html doc.");
 }
 
@@ -13,4 +14,4 @@ ReactDOM.render(
     <App />
   </StrictMode>,
   rootElement
-)
+);
