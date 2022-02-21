ce584438a13c7613628b7fbe4590b780441c5efc
AionChiu <30471173+chingQAQ@users.noreply.github.com>
2022-02-21 14:30:44 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
+import svgr from '@honkhonk/vite-plugin-svgr';
 import path from "path";
 
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [reactRefresh(), svgr()],
   resolve: {
     alias: {
       "@": `${path.resolve(__dirname, "src")}`,
